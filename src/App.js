d28dd67f3b7ca7674a6859a6a80e158e8911b1a2
badoalmaz <85788119+badoalmaz@users.noreply.github.com>
2022-02-18 13:54:46 +0600

    firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import AuthContextProvider from './contexts/AuthContext';
 import MainRoutes from './MainRoutes';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <MainRoutes />
-      </BrowserRouter>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <MainRoutes />
+        </BrowserRouter>
+      </AuthContextProvider>
     </>
   );
 }
