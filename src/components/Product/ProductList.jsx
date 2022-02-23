107f525fcc2f5a2c653021fd6218e1cc94824a1e
badoalmaz <85788119+badoalmaz@users.noreply.github.com>
2022-02-23 12:20:28 +0600

    added search

--- src/components/Product/ProductList.jsx
+++ src/components/Product/ProductList.jsx
@@ -1,5 +1,6 @@
-import { Grid } from '@mui/material';
-import React, { useEffect } from 'react';
+import { Grid, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { useProducts } from '../../contexts/ProductContext';
 import MediaCard from '../Product/ProductCard';
 
@@ -7,25 +8,52 @@ import ProductCard from '../Product/ProductCard';
 
 const ProductList = () => {
   const { products, getProducts } = useProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [search, setSearch] = useState(
+    searchParams.get('q') ? searchParams.get('q') : ''
+  );
+
   useEffect(() => {
     getProducts();
+  }, [searchParams]);
+
+  useEffect(() => {
+    setSearchParams({
+      q: search,
+    });
   }, []);
 
+  useEffect(() => {
+    setSearchParams({
+      q: search,
+    });
+  }, [search]);
+
   return (
-    <Grid container spacing={6}>
-      {products ? (
-        products.map((item) => (
-          <Grid item>
-            {/* <ProductCard item={item} key={item.id} /> */}
-            <MediaCard item={item} key={item.id} />
-          </Grid>
-        ))
-      ) : (
-        <>
-          <h2>..Loading</h2>
-        </>
-      )}
-    </Grid>
+    <>
+      <TextField
+        label="Search"
+        variant="standard"
+        sx={{ margin: '2vw', width: '35vw' }}
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <Grid container spacing={6}>
+        {products ? (
+          products.map((item) => (
+            <Grid item>
+              {/* <ProductCard item={item} key={item.id} /> */}
+              <MediaCard item={item} key={item.id} />
+            </Grid>
+          ))
+        ) : (
+          <>
+            <h2>..Loading</h2>
+          </>
+        )}
+      </Grid>
+    </>
   );
 };
 
