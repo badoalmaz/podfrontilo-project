921eda55b594dec99d163a315377beb1021a3802
Duishobaev <seyitbekaigu@gmail.com>
2022-02-24 13:01:14 +0600

    added product-details

--- src/components/Product/ProductDetails.jsx
+++ src/components/Product/ProductDetails.jsx
@@ -1,30 +1,34 @@
 import React from "react";
 import { useEffect } from "react";
-
 import { useParams } from "react-router-dom";
 import { useProducts } from "../../contexts/ProductContext";
+import "./ProductDetails.css";
 
 const ProductDetails = () => {
-	const { id } = useParams();
-	const { getProductDetails, productDetails } = useProducts();
+  const { id } = useParams();
+  const { getProductDetails, productDetails } = useProducts();
 
-	useEffect(() => {
-		getProductDetails(id);
-	}, [id]);
+  useEffect(() => {
+    getProductDetails(id);
+  }, [id]);
 
-	return (
-		<div style={{ display: "flex" }}>
-			<div>
-				<img src={productDetails.picture} alt="" />
-			</div>
-			<div>
-				<h2>name:{productDetails.name}</h2>
-				<h3>type:{productDetails.type}</h3>
-				<h4>price:{productDetails.price}</h4>
-				<h5>description:{productDetails.description}</h5>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="details-wrap"
+      style={{ display: "flex", justifyContent: "space-evenly" }}
+    >
+      <div>
+        <img id="details-img" src={productDetails.picture} alt="" />
+      </div>
+
+      <div className="details-list">
+        <h2 className="title">{productDetails.name}</h2>
+        <h3 className="type">{productDetails.type}</h3>
+        <h4 className="price">{productDetails.price}</h4>
+        <h5 className="descr">{productDetails.description}</h5>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
