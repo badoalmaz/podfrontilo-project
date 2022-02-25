fa45ac7a58bf4ca1a0558127d8db9d7b38d685cf
Aizhamal Sulaimanova <aijamal.sulaimanova.kk.@gmail.com>
2022-02-25 15:30:59 +0600

    sidebar

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -7,19 +7,27 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useProducts } from "../../contexts/ProductContext";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts/AuthContext";
 import FavoriteSharpIcon from "@mui/icons-material/FavoriteSharp";
 import ShoppingBasketSharpIcon from "@mui/icons-material/ShoppingBasketSharp";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import "./styles/ProductCard.css";
 
 import { Delete, Edit } from "@mui/icons-material";
+import { ADMIN } from "../../helpers/consts";
+import { IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 export default function MediaCard({ item }) {
-  const { deleteProduct } = useProducts();
+  const { deleteProduct, addProductToCart, checkProductInCart } = useProducts();
   const navigate = useNavigate();
 
+  const {
+    user: { email },
+  } = useAuth();
+
   return (
-    <div>
+    <>
       <Card sx={{ borderRadius: 3 }}>
         <div className="cardProduct">
           <CardMedia
@@ -37,93 +45,29 @@ export default function MediaCard({ item }) {
               {item.description}
             </Typography>
           </CardContent>
-          <CardActions>
-            <Button
-              size="small"
-              disabled
-              onClick={() => deleteProduct(item.id)}
-            >
-              <Delete />
-            </Button>
-            <Button
-              size="small"
-              disabled
-              onClick={() => navigate(`/edit/${item.id}`)}
-            >
-              <Edit />
-            </Button>
-            <div className="basket">
-              <span>{<ShoppingBasketSharpIcon />}</span>
-              <span>{<FavoriteSharpIcon />}</span>
-              <span onClick={() => navigate(`/products/${item.id}`)}>
-                <ArrowRightAltIcon />
-              </span>
-            </div>
-          </CardActions>
         </div>
+        <CardActions>
+          {email === ADMIN ? (
+            <>
+              {" "}
+              <Button size="small" onClick={() => deleteProduct(item.id)}>
+                DELETE
+              </Button>
+              <Button size="small" onClick={() => navigate(`/edit/${item.id}`)}>
+                EDIT
+              </Button>
+            </>
+          ) : (
+            <IconButton onClick={() => addProductToCart(item)}>
+              <ShoppingCartIcon
+                color={checkProductInCart(item.id) ? "secondary" : ""}
+              />
+            </IconButton>
+          )}
+
+          <span onClick={() => navigate(`/products/${item.id}`)}>more...</span>
+        </CardActions>
       </Card>
-    </div>
+    </>
   );
 }
-// import * as React from 'react';
-// import Card from '@mui/material/Card';
-// import CardActions from '@mui/material/CardActions';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Button from '@mui/material/Button';
-// import Typography from '@mui/material/Typography';
-// import { useNavigate } from 'react-router-dom';
-// import { useProducts } from '../../contexts/ProductContext';
-// import { useAuth } from '../../contexts/AuthContext';
-// import { ADMIN } from '../../helpers/consts';
-// import { IconButton } from '@mui/material';
-// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-
-// export default function MediaCard({ item }) {
-//   const { deleteProduct, addProductToCart, checkProductInCart } = useProducts();
-//   const {
-//     user: { email },
-//   } = useAuth();
-
-//   const navigate = useNavigate();
-
-//   return (
-//     <Card sx={{ maxWidth: 345 }}>
-//       <CardMedia
-//         component="img"
-//         height="140"
-//         image={item.picture}
-//         alt="green iguana"
-//       />
-//       <CardContent>
-//         <Typography gutterBottom variant="h5" component="div">
-//           {item.name}
-//         </Typography>
-//         <Typography variant="body2" color="text.secondary">
-//           {item.description}
-//         </Typography>
-//       </CardContent>
-//       <CardActions>
-//         {email === ADMIN ? (
-//           <>
-//             {' '}
-//             <Button size="small" onClick={() => deleteProduct(item.id)}>
-//               DELETE
-//             </Button>
-//             <Button size="small" onClick={() => navigate(`/edit/${item.id}`)}>
-//               EDIT
-//             </Button>
-//           </>
-//         ) : (
-//           <IconButton onClick={() => addProductToCart(item)}>
-//             <ShoppingCartIcon
-//               color={checkProductInCart(item.id) ? 'secondary' : ''}
-//             />
-//           </IconButton>
-//         )}
-
-//         <span onClick={() => navigate(`/products/${item.id}`)}>more...</span>
-//       </CardActions>
-//     </Card>
-//   );
-// }
